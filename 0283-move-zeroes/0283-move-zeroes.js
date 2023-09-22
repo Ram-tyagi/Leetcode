82267964f94821e7888c8e9a93e6fdae50721476
Ram Tyagi <40211634+Ram-tyagi@users.noreply.github.com>
2023-09-22 23:33:05 +0530

    Time: 93 ms (18.82%), Space: 46.3 MB (80.44%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+   let size = nums.length;
+     for (let i = 0; i < size; i++) {
+         if (nums[i] === 0) {
+             nums.splice(i, 1);
+             nums.push(0);
+             i--;
+             size--;
+         }
+     }
+    return nums;
+};
