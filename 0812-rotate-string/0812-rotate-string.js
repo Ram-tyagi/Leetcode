41abb55e7a2792cdcf431e9b4d126821e754da73
Ram Tyagi <40211634+Ram-tyagi@users.noreply.github.com>
2023-09-29 23:49:05 +0530

    Time: 60 ms (11.38%), Space: 41.7 MB (76.18%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function(s, goal) {
+      if(goal.length !== s.length) return false 
+
+    let str = s+s
+
+    return str.includes(goal)
+};
