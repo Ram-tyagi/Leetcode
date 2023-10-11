0c4d6f8a8cc0af9444755d199eb59b91c40c595c
Ram Tyagi <40211634+Ram-tyagi@users.noreply.github.com>
2023-10-11 23:50:02 +0530

    Time: 163 ms (88.43%), Space: 51.2 MB (98.02%) - LeetHub

---
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.js
@@ -0,0 +1,23 @@
+
+var RecentCounter = function() {
+     this.stream = []
+};
+
+/** 
+ * @param {number} t
+ * @return {number}
+ */
+RecentCounter.prototype.ping = function(t) {
+    this.stream.push(t)
+
+     while(this.stream[0] < t - 3000){ 
+      this.stream.shift()
+  } 
+    return this.stream.length
+};
+
+/** 
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */
