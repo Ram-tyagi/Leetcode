2c263099eea29a0ea5944f461baa99f9e093497a
Ram Tyagi <40211634+Ram-tyagi@users.noreply.github.com>
2023-10-21 23:16:33 +0530

    Time: 61 ms (27.98%), Space: 42.3 MB (66.33%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+     let Parenthesis = { 
+     "(": ")",
+     "[": "]",
+     "{": "}"
+}
+
+   let stack = []
+
+  for(let char of s){
+    if(Parenthesis[char]){
+      stack.push(char);
+    }else{
+      let top = stack.pop(char)
+
+      if(char !== Parenthesis[top]){
+        return false;
+      }
+    }
+  }
+   return stack.length === 0;
+};
