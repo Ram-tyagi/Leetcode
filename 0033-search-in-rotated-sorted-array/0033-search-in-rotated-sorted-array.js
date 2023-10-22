00b64d85e45e812a4aefaeab7e1a4697b321cfaf
Ram Tyagi <40211634+Ram-tyagi@users.noreply.github.com>
2023-10-23 00:39:49 +0530

    Time: 57 ms (27.95%), Space: 42.2 MB (36.77%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,12 +4,33 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]===target){
-            return i;
-        }
-       
-    }
-    return -1;
+   let left=0
+   right=nums.length-1;
+   while(left<=right){
+       let mid=Math.floor(left+(right-left)/2)
+       if(nums[mid]===target){
+        return mid;
+       }
+       if(nums[left]<=nums[mid]){
+           if(nums[left]<=target && target<nums[mid]){
+               right=mid-1
+           }
+           else{
+               left=mid+1
+           }
+
+       }
+       else{
+           if(nums[mid]<target && target<=nums[right]){
+               left=mid+1
+           }
+           else{
+               right=mid-1
+           }
+       }
+      
+   }
+      
+   return -1
     
 };
