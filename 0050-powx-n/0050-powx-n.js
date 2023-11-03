49bc295f55984ca886a475af1b7c197c435523db
Ram Tyagi <40211634+Ram-tyagi@users.noreply.github.com>
2023-11-03 23:53:17 +0530

    Time: 64 ms (16.47%), Space: 42.3 MB (21.31%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    if (n==0){
+        return 1;
+    }
+    if(n<0){
+        x=1/x
+        n=-n;
+    }
+    if(n%2==0){
+        let power=myPow(x,n/2)
+        return power*power
+    }
+    else{
+        return x*myPow(x,n-1)
+    }
+};
