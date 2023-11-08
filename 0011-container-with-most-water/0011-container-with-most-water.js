3030e8fe0fe73f2e42d1506b8237d62b0d2aa625
Ram Tyagi <40211634+Ram-tyagi@users.noreply.github.com>
2023-11-08 23:48:25 +0530

    Time: 79 ms (10.2%), Space: 48.9 MB (95.69%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+   
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
+    while(left < right)
+    {
+        let area = Math.min(height[left], height[right]) * (right -left);
+
+        if(area > maxArea)
+        {
+            maxArea = area;
+        }
+
+        if(height[left] < height[right])
+        {
+            left++;
+        }
+        else
+        {
+            right--;
+        }
+    }
+
+    return maxArea;
+      
+};
